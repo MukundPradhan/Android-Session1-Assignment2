ed993993d5d3ce4fa193c8b2b6ed60e5bc512502
MukundPradhan <replyme.mmp@gmail.com>
2016-02-24 15:55:18 +0530

    Initial commit

---
+++ app/src/main/java/com/mukund/android_session1_assignment2/MainActivity.java
@@ -0,0 +1,21 @@
+/*
+This program is to be Run in bluestak
+ */
+
+
+
+
+
+package com.mukund.android_session1_assignment2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
